bd5b90e22fd38528b01ee8c65f5dde794d27f81f
katebennu <kate.dorrer@gmail.com>
2017-03-24 23:05:10 +0200

    render progress bar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.jpg';
 import './App.css';
 //import './progress.js'
-import ProgressBar from '../node_modules/progressbar.js';
-// var ProgressBar = require('progressbar.js');
+
 
 
 class App extends Component {
@@ -28,23 +27,8 @@ class App extends Component {
   }
 }
 
-var container = document.getElementById('container');
 
-var bar = new ProgressBar.Line(container, {
-    strokeWidth: 4,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#FFEA82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'},
-    from: {color: '#FFEA82'},
-    to: {color: '#ED6A5A'},
-    step: (state, bar) => {
-        bar.path.setAttribute('stroke', state.color);
-    }
-});
 
-bar.animate(0.7);
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+var ProgressBar = require('progressbar.js');
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+var bar = new ProgressBar.Line('#container', {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'},
+    from: {color: '#FFEA82'},
+    to: {color: '#ED6A5A'},
+    step: (state, bar) => {
+        bar.path.setAttribute('stroke', state.color);
+    }
+});
+
+bar.animate(0.7);
