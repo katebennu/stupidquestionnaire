d0c3f2747206a7c95fa0c5982b48077d85f04e95
katebennu <kate.dorrer@gmail.com>
2017-03-24 21:25:04 +0200

    outline app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Which Game of Thrones character are you?</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3 className="App-intro">
+         Answer a few questions and see the results!
+        </h3>
+          <p>Questions shall appear here</p>
+          <input/>
+          <button>Totally!</button>
+          <div>---progress bar---</div>
       </div>
     );
   }
